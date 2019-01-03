6192aad9013fe71cb415834c3f75997cfcafde51
Daniel Perez <pereznetworks@live.com>
2019-01-03 15:28:32 -0800

    changed some demo code

--- WeatherX/src/Components/mainContents.js
+++ WeatherX/src/Components/mainContents.js
@@ -10,15 +10,22 @@ class MainContents extends Component {
       <div id="main-content">
         <h2>WeatherX UI Working Demo</h2>
         <h3>CSS Template using Grid</h3>
-        <p>In this example, we have created a left, a middle and a right.</p>
-        <p>This layout is more RWD</p>
-        <p>Better than header-middle-footer grid:</p>
-        <p>Logo and Title dont change when screen or browser resized</p>
-        <p>We will also be testing our 'flat' UX design, using flat icons and flat stlying of menus and buttons</p>
-        <p>Featuring a Z-index Logo of +1, and titleBar and main content div</p>
-        <p>some Javascript is used to demo a the drop-menu, home/back and form navigation </p>
-        <p>A lot of tweeking has been done to get layout, element positioning, etc. to be just right.</p>
-        <p> <strong>This is just a layout demo. Now the tough job; to port and implement in ReactJS</strong> </p>
+        <p>
+          In this example, we have created a left, a middle and a right.<br />
+          Only the middle grid area is used<br />
+          This layout is more RWD<br />
+          Better than header-middle-footer grid:<br />
+          Logo and Title dont change when screen or browser resized<br />
+          We will also be testing our 'flat' UX design,<br />
+          using flat icons and flat stlying of menus and buttons<br />
+          Featuring a Z-index Logo of +1, and titleBar and main content div<br />
+          some Javascript is used<br />
+          to demo a the drop-menu, home/back and form navigation <br />
+          A lot of tweeking has been done... <br />
+          to get layout, element positioning, etc. to be just right.<br />
+          <br /><strong>This is just a layout demo.<br />
+           Now the tough job; to port and implement in ReactJS</strong><br />
+        </p>
       </div>
     );
   }
