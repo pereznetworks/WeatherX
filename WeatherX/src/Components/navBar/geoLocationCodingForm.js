3dfc3d03467699751320981be6f4271a2e63815d
Daniel Perez <pereznetworks@live.com>
2019-01-08 19:35:18 -0800

    tweeking navbar ui to for mobile first

--- WeatherX/src/Components/navBar/geoLocationCodingForm.js
+++ WeatherX/src/Components/navBar/geoLocationCodingForm.js
@@ -24,25 +24,25 @@ export default class GeoLocationCodingForm extends Component {
               <p
                  id="geoLocation-Label"
                  style={{display:"inline"}}
-                 onClick={this.props.handleNavSubmit}>To enable geolocation services, click here -></p>
+                 onClick={this.props.handleNavSubmit}>We'll find you, click here -></p>
               <input
                  type="button"
                  id="geoLocation-submit"
                  style={{display:"inline"}}
                  onClick={this.props.handleNavSubmit}/>
-              <input type="text" id="geoCoding-input" placeholder="Enter a location & click here ->" style={{display:"none"}}/>
+              <input type="text" id="geoCoding-input" placeholder="Enter a location" style={{display:"none"}}/>
               <input type="button" id="geoCoding-submit" style={{display:"none"}}/>
             </form>
         );
     } else if (this.props.navState.geoLocationCodingForm === true && this.props.navState.geoCoding === true ){
       return(
             <form id="geoLocationCoding-Form" style={{display:"inline"}}>
-              <p id="geoLocation-Label" style={{display:"none"}}>To enable geolocation services, click here -></p>
+              <p id="geoLocation-Label" style={{display:"none"}}>We'll find you, click here -></p>
               <input type="button" id="geoLocation-submit" style={{display:"none"}}/>
               <input
                     type="text"
                     id="geoCoding-input"
-                    placeholder="Enter a location & click here ->"
+                    placeholder="Enter a location"
                     style={{display:"inline"}}
                     onClick={this.props.handleNavSubmit}/>
               <input
