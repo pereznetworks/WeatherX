aa75e89872ed7d8ac6d51a8090c374b74923c367
Daniel Perez <pereznetworks@live.com>
2019-02-05 09:17:44 -0800

    resolved reactjs warnings

--- WeatherX/src/Components/mainView/tableWeek/dailyConditions.js
+++ WeatherX/src/Components/mainView/tableWeek/dailyConditions.js
@@ -5,32 +5,35 @@ export default class DailyConditions extends Component {
   constructor(props){
     super(props)
     this.selectDailyConditions = this.selectDailyConditions.bind(this);
+    this.setDailyConditionElement = this.setDailyConditionElement.bind(this);
   }
 
-  selectDailyConditions(){
-    return this.props.navState.dailyConditions.map((object, index) => {
-            if (index === this.props.dayIndex){
-              if ( object.icon === 'cloudy'){
-                return <i className="wi wi-day-cloudy"></i>
-              } else if ( object.icon === 'fog'){
-                return <i className="wi wi-day-fog"></i>
-              } else if ( object.icon === 'partly-cloudy-day' ||  object.icon === 'partly-cloudy-night'  ){
-                return <i className="wi wi-day-sunny-overcast"></i>
-              } else if ( object.icon === 'rain'){
-                return <i className="wi wi-day-rain"></i>
-              } else if ( object.icon === 'clear' ||  object.icon === 'clear-night' ||  object.icon === 'clear-day'){
-                return <i className="wi wi-day-sunny"></i>
-              } else if ( object.icon === 'snow'){
-                return <i className="wi wi-day-snow"></i>
-              } else if ( object.icon === 'scattered-showers'){
-                return <i className="wi wi-day-showers"></i>
-              } else if ( object.icon === 'thunder'){
-                return <i className="wi wi-day-thunderstorm"></i>
-              } else if ( object.icon === 'wind'){
-                return <i className="wi wi-day-windy"></i>
-              }
+  setDailyConditionElement(object, index){
+          if (index === this.props.dayIndex){
+            if ( object.icon === 'cloudy'){
+              return <i key={index} className="wi wi-day-cloudy"></i>
+            } else if ( object.icon === 'fog'){
+              return <i key={index} className="wi wi-day-fog"></i>
+            } else if ( object.icon === 'partly-cloudy-day' ||  object.icon === 'partly-cloudy-night'  ){
+              return <i key={index}  className="wi wi-day-sunny-overcast"></i>
+            } else if ( object.icon === 'rain'){
+              return <i key={index} className="wi wi-day-rain"></i>
+            } else if ( object.icon === 'clear' ||  object.icon === 'clear-night' ||  object.icon === 'clear-day'){
+              return <i key={index} className="wi wi-day-sunny"></i>
+            } else if ( object.icon === 'snow'){
+              return <i key={index} className="wi wi-day-snow"></i>
+            } else if ( object.icon === 'scattered-showers'){
+              return <i key={index} className="wi wi-day-showers"></i>
+            } else if ( object.icon === 'thunder'){
+              return <i key={index} className="wi wi-day-thunderstorm"></i>
+            } else if ( object.icon === 'wind'){
+              return <i key={index} className="wi wi-day-windy"></i>
             }
-          })
+          }
+        }
+
+  selectDailyConditions(){
+    return this.props.navState.dailyConditions.map(this.setDailyConditionElement)
     }
 
   render(){
--- WeatherX/src/Components/mainView/tableWeek/index.js
+++ WeatherX/src/Components/mainView/tableWeek/index.js
@@ -12,14 +12,14 @@ export default class TableWeek extends Component {
               <th id="tempHigh"></th>
               <th id="tempLow"></th>
             </tr>
-            <DailyConditions navState={this.props.navState} dayIndex={0}/>
-            <DailyConditions navState={this.props.navState} dayIndex={1}/>
-            <DailyConditions navState={this.props.navState} dayIndex={2}/>
-            <DailyConditions navState={this.props.navState} dayIndex={3}/>
-            <DailyConditions navState={this.props.navState} dayIndex={4}/>
-            <DailyConditions navState={this.props.navState} dayIndex={5}/>
-            <DailyConditions navState={this.props.navState} dayIndex={6}/>
-            <DailyConditions navState={this.props.navState} dayIndex={7}/>
+            <DailyConditions key={0} navState={this.props.navState} dayIndex={0}/>
+            <DailyConditions key={1} navState={this.props.navState} dayIndex={1}/>
+            <DailyConditions key={2} navState={this.props.navState} dayIndex={2}/>
+            <DailyConditions key={3} navState={this.props.navState} dayIndex={3}/>
+            <DailyConditions key={4} navState={this.props.navState} dayIndex={4}/>
+            <DailyConditions key={5} navState={this.props.navState} dayIndex={5}/>
+            <DailyConditions key={6} navState={this.props.navState} dayIndex={6}/>
+            <DailyConditions key={7} navState={this.props.navState} dayIndex={7}/>
         </tbody>
       </table>
     );
