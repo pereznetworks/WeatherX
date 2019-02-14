231e927a1a7041d4a84e7e5b3b38d5571612b0de
Daniel Perez <pereznetworks@live.com>
2019-02-13 22:31:30 -0800

    Update index.js

--- WeatherX/src/Components/locationBar/index.js
+++ WeatherX/src/Components/locationBar/index.js
@@ -115,16 +115,3 @@ export default class LocationBar extends Component {
       }
   }
 }
-
-/*
-<div title="locationBar" id="locationBar-div" onClick={this.props.handleNavClick}>
-  <div title="locationBar" id="cityTime-div" onClick={this.props.handleNavClick}>
-    <p title="locationBar" id="locationTime" onClick={this.props.handleNavClick}>9:00 PM</p>
-    <p title="locationBar" id="locationName" onClick={this.props.handleNavClick}>{this.props.navState.currentLocation}</p>
-  </div>
-  <div title="locationBar" id="temp-div" onClick={this.props.handleNavClick}>
-    <p  title="locationBar" id="currentTemp" onClick={this.props.handleNavClick}>61°</p>
-  </div>
-</div>
-
- */
