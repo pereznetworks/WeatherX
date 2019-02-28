4e1f79ffa57b719de51adde2aa5adad4df20fc95
Daniel Perez <pereznetworks@live.com>
2019-02-28 12:44:53 -0800

    getting ready to switch from Mongod to Sequelize/sqlite3

---
+++ sequelizeVer/dataSource/forecast/index.js
@@ -0,0 +1,91 @@
+// methods for access api
+
+const current = {
+  mostRecentForecast: {},
+  mostRecentLocation: {}
+}
+const db = {
+  locationArray: [],
+  forecastArray: []
+}; // will need to replace this with mongoose code
+
+const manageLocData = loc => {
+  if (loc){
+    if (current.mostRecentLocation){ // push current forecast onto array
+      db.locationArray.push(current.mostRecentLocation);
+    }
+    current.mostRecentLocation = {  // save new current foreeast
+      timeStamp: Date.now(),
+      data: loc
+    } // will end up with an array of objects: [{timeStamp:<dateInt>, data:forecast.json}]
+    return current;
+  }
+}; // will need to replace this with mongoose code also
+
+
+const manageForecastData = forecast => {
+  if (forecast){
+    if (current.mostRecentForecast){ // push current forecast onto array
+      db.forecastArray.push(current.mostRecentForecast);
+    }
+    current.mostRecentForecast = {  // save new current foreeast
+      timeStamp: Date.now(),
+      data: forecast
+    } // will end up with an array of objects: [{timeStamp:<dateInt>, data:forecast.json}]
+    return current;
+  }
+}; // will need to replace this with mongoose code also
+
+// function to make a forcast.io api call for forecaast data
+const getForecastApiCall = coordinates => {
+
+  const key = require('../config').forecastKey;
+  // will need to import mapbox module and key also
+
+  // fail-safe in-case no valid coordinates are passed
+  if (!coordinates.latitude || !coordinates.longitude) {
+    return coordinates.notice;
+  }
+
+  let apicall = `https://api.darksky.net/forecast/${key}/${coordinates.latitude},${coordinates.longitude}`;
+  return apicall;
+
+};
+
+const getGeoCodeApiCall = searchTerm => {
+
+  const geoCodeKey = require('../config').geoCodeKey;
+  let searchTermArray;
+  let coordinates = {};
+
+  // due to cost may want to not use up apicalls to sources for every invalid respsonse
+  // need to parse req.param.locaton for valid city, state, and or zip-codes
+  // ONLY IF using TomTom's structured geocode search
+  let lookForCommaBetween = /,(?=[\sA-Za-z])/g;
+  let lookForWords = /[A-Za-z]\w+/g;
+  let alphanumberic = /\w+/g;
+  let lookforCountry = /(?<=,)[A-Za-z]\w+/g;
+  let lookforZipCode = /^\d+$/g;
+  let lookForForeignPostalCode = /([A-Za-z0-9])\w+([\s])\w+/g;
+  let countryCode = `US`;  // setting default
+  let cityName;
+
+  // using TomTom unstructed geocode search, just make sure searchTerm isn't blank
+  if (!searchTerm){
+    coordinates.notice = `Opps`;
+  } else {
+    return `https://api.tomtom.com/search/2/geocode/${searchTerm}.json?limit=1&key=${geoCodeKey}`
+  }
+
+  // uncomment below for testing only
+  // if(searchTerm.location !== 'San Francisco, CA'){
+  //   coordinates.longitude = -122.420679;
+  //   coordinates.latitude = 37.772537;
+  // }
+  // return coordinates;
+};
+
+module.exports.getForecastApiCall= getForecastApiCall;
+module.exports.getGeoCodeApiCall = getGeoCodeApiCall;
+module.exports.manageForecastData = manageForecastData;
+module.exports.manageLocData = manageLocData;
---
+++ sequelizeVer/index.js
@@ -0,0 +1,68 @@
+// WeatherX server
+
+'use strict'; // makes sure, among other things, that no unneeded errors creep into my code
+
+// importing Express, and set the port it will accept connections on
+const express = require('express');
+// creating an Express 'server', don't want to confuse with 'front-end' part of this project
+const server = express();
+
+// for use when deploying into production, otherwise use env='development' and port='3000'
+const env = process.env.NODE_ENV || 'development';
+const port = process.env.PORT || 9999;
+
+// setting up a routes module
+const routes = require('./routes/index.js');
+
+// require modules...
+const jsonParser = require('body-parser').json; // importing body-parser's json method for parsing JSON data
+const logger = require('morgan'); // import a middleware logger module.. see NPM's Morgan PKG page for details
+
+// connect modules to express server, make available to all other Express server.methods
+server.use(jsonParser()); // parse all req.body data as json and
+server.use(logger('dev')); // Concise output colored by response status for development use.
+
+// secruity measures
+const helmet = require('helmet')
+server.use(helmet());
+
+// allow requests from specific host
+server.use((req, res, next) => {
+	res.append('Access-Control-Allow-Origin', ['http://10.100.10.102:3000']);
+	res.append('Access-Control-Allow-Methods', ['GET']);
+	res.append('Access-Control-Allow-Headers', ['Content-Type']);
+	next();
+})
+
+// sequelize stuff here ...
+
+// insert custom routes router here
+server.use(routes);
+server.use('/', routes);
+server.use('/weather', routes);
+
+
+// prevent favicon errors, may include a real favicon eventually
+server.get('/favicon.ico', (req, res, next) => res.status(204));
+
+// catch http 404 status code and forward to error handler
+server.use(function(req, res, next){
+	const err = new Error("Not Found");
+	err.status = 404;
+	next(err);
+});
+
+// Error Hander
+server.use(function(err, req, res, next){
+		res.status(err.status || 500);
+		res.json({
+			error: {
+				message: err.message
+			}
+		});
+});
+
+// start the server
+server.listen(port, function(){
+	console.log('Express server listening on port', port);
+});
