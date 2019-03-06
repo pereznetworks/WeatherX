8627c6ee0ed685d73e4586a456cb03d6cb7dd6a7
DAP <admin@Admins-MBP.hsd1.ca.comcast.net>
2019-03-05 18:41:58 -0800

    Update index.js

--- WeatherX-Server/index.js
+++ WeatherX-Server/index.js
@@ -6,7 +6,6 @@ var throng = require('throng');
 // 2: heroku dev docs indicate to check for process.env.WEB_CONCURRENCY
 // also checking for process.env.PORT
 const CONCURRENCY = process.env.WEB_CONCURRENCY || 1;
-const port = process.env.PORT || 9999;
 
 // 3: setup a startFunction, imports the function to start the server
 // using this method, each 'cluster worker', will start server that has it's own exclusive database
