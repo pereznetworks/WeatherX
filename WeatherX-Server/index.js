314fdae7dfc2150126cf51410bcdc35f2cf18898
DAP <admin@Admins-MBP.hsd1.ca.comcast.net>
2019-03-05 18:18:26 -0800

    using THRONG for clustering

--- WeatherX-Server/index.js
+++ WeatherX-Server/index.js
@@ -1,79 +1,29 @@
-// WeatherX server
+// Heroku deployment indicates that built-in clustering is BEST PRACTICE
+// some of the options are ....
+// CLUSTER
+// ... looks like a great cluster abstraction but,
+// ... does not run on node > v4, and hasn't updated in 2012
+// ... perhaps can look into forking the GITHUB repo for it and updating it
+// the FORKY module requires
+// ... embedding calls to node's cluster module in your code
+// ... manual management of processes
+// so for now ...
+// using the THRONG for cluster abstraction, even though last update was 2016
+// but using seems more straight-forward
 
-'use strict'; // makes sure, among other things, that no unneeded errors creep into my code
+// 1: after installing it, require it....
+var throng = require('throng');
 
-// importing Express, and set the port it will accept connections on
-const express = require('express');
-
-// using sequelize for data ...
-// importing from models/index.js
-// .. which sets config, checks for or creates a db,
-// ... some other housekeeping and model importing into sequelize
-// .... then imports here
-const sequelize = require('./data/models').sequelize;
-
-// creating an Express 'server', don't want to confuse with 'front-end' part of this project
-const server = express();
-
-// for use when deploying into production, otherwise use env='development' and port='3000'
-const env = process.env.NODE_ENV || 'development';
+// 2: heroku dev docs indicate to check for process.env.WEB_CONCURRENCY
+// also checking for process.env.PORT
+const CONCURRENCY = process.env.WEB_CONCURRENCY || 1;
 const port = process.env.PORT || 9999;
 
-// setting up a routes module
-const routes = require('./routes/index.js');
+// 3: setup a startFunction, imports the function to start the server
+// using this method, each 'cluster worker', will start server that has it's own exclusive database
+// this is okay, because the api sources used by this server, require no data be saved or stored
+// so each server's seqeulize-sqlite-model-instances are destroyed after res.json(data)
+const startFunction = require('./server.js');
 
-// require modules...
-const jsonParser = require('body-parser').json; // importing body-parser's json method for parsing JSON data
-const logger = require('morgan'); // import a middleware logger module.. see NPM's Morgan PKG page for details
-
-// connect modules to express server, make available to all other Express server.methods
-server.use(jsonParser()); // parse all req.body data as json and
-server.use(logger('dev')); // Concise output colored by response status for development use.
-
-// secruity measures
-const helmet = require('helmet')
-server.use(helmet());
-
-// allow requests from specific host
-server.use((req, res, next) => {
-	res.append('Access-Control-Allow-Origin', ['http://10.100.10.102:3000']);
-	res.append('Access-Control-Allow-Methods', ['GET']);
-	res.append('Access-Control-Allow-Headers', ['Content-Type']);
-	next();
-});
-
-// insert custom routes router here
-server.use(routes);
-server.use('/', routes);
-server.use('/weather', routes);
-
-
-// prevent favicon errors, may include a real favicon eventually
-server.get('/favicon.ico', (req, res, next) => res.status(204));
-
-// catch http 404 status code and forward to error handler
-server.use((req, res, next) => {
-	const err = new Error("Not Found");
-	err.status = 404;
-	next(err);
-});
-
-// Error Hander
-server.use((err, req, res, next) => {
-		res.status(err.status || 500);
-		res.json({
-			error: {
-				message: err.message
-			}
-		});
-});
-
-// sync the database, then start the server
-sequelize
-	.sync()
-	.then(
-			function(){
-				server.listen(port, function(){
-					console.log('Express server listening on port', port);
-				});
-			});
+// 4: use throng to start CONCURRENCY number of servers, all listening on the same port
+throng(CONCURRENCY, startFunction);
