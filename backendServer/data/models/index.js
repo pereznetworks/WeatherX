755a35bf5cd590ef44dde0d422a019d8db02993b
Daniel Perez <pereznetworks@live.com>
2019-04-10 14:23:38 -0700

    cleaning up sequelize setup

--- backendServer/data/models/index.js
+++ backendServer/data/models/index.js
@@ -1,42 +1,22 @@
 'use strict';
 
-// code courtest\y Team Treehouse, from FSJS Project 10
-
-// yes, will be parsing folder paths, loading and reading files
-// const fs = require('fs');
-// const path = require('path');
+// code courtesy Team Treehouse, from FSJS Project 10
+// removed fs, path and process.env
+// dont need a db file, wont be storing data long term
 
 // get sequelize environment variables
 const Sequelize = require('sequelize');
-// const basename = path.basename(__filename);
-// const env = process.env.NODE_ENV || 'development';
 
+// will have to figure out how to pass production environment variables ...
+// const env = process.env.NODE_ENV || 'development';
 const env = 'development';
 const config = require('../config/' + 'config.json')[env];
-const db = {};
 
 // set seqeulize so it can accesss the db using settings from config.json
 let sequelize = new Sequelize(config.database, config.username, config.password, config);
 
-// let sequelize;
-// if (config.use_env_variable) {
-//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-// } else {
-//   sequelize = new Sequelize(config.database, config.username, config.password, config);
-// }
-
-// use the settings captured above to check for existence of a db file
-// fs
-//   .readdirSync(__dirname)
-//   .filter(file => {
-//     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-//   })
-//   .forEach(file => {
-//     const model = sequelize['import'](path.join(__dirname, file));
-//     db[model.name] = model;
-//   });
-
 // create a db object that matches the tables created from model definitions
+const db = {}; 
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
